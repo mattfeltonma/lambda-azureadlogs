9eaba1a4bfaae75e049c9b6f8888404282b6e097
Matt Felton <matt.felton@feltonma.com>
2019-01-28 02:02:07 +0000

    Initial commit Created CF templates and build shell script

---
+++ setup.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+## User variables
+##
+REGION="us-east-1" #Region resources should be pushed
+BUCKET="mybucket" #Add S3 Bucket here where logs will go
+
+# Role or user that will have permission over new AWS KMS CMK
+PRINCIPALARN="arn:aws:iam::<ACCNT>:<ROLE/USER>"
+
+TENANTNAME="mytenant.com" #Enter Azure AD tenant name
+CLIENTID="11111111-1111-1111-1111-111111111111" #Azure AD Client ID
+CLIENTSECRET="XXXXXXXXXXXXXXX" #Azure AD Client Secret
+
+## Permanent variables
+##
+SIGNIN="signinlogs.py"
+AUDIT="auditlogs.py"
+
+## Setup build directory
+##
+mkdir build
+cd build
+pip install adal --target .
+
+## Create package
+##
+zip -r9 ../lambda-signin.zip .
+cd ../
+cp lambda-signin.zip lambda-audit.zip
+zip -g lambda-signin.zip $SIGNIN
+zip -g lambda-audit.zip $AUDIT
+
+## Execute CloudFormation
+##
+aws cloudformation create-stack --stack-name azureadlogs \
+--template-body file://cf-templates/cf-resources.yaml \
+--capabilities CAPABILITY_NAMED_IAM \
+--parameters ParameterKey=LogBucket,ParameterValue=$BUCKET \
+ParameterKey=KMSAdminArn,ParameterValue=$PRINCIPALARN \
+ParameterKey=TenantName,ParameterValue=$TENANTNAME --region $REGION
+
+echo "Waiting for stack to complete.."
+aws cloudformation wait stack-create-complete --stack-name azureadlogs --region $REGION
+## Get role ARN
+##
+
+RESULT=`aws iam get-role --role-name Custom-Lambda-AzureADLogs --query Role.Arn`
+ROLE=`echo $RESULT | sed 's/"//g'`
+
+## Create Lambda function
+##
+lambda_function.lambda_handler
+
+
+aws lambda create-function --function-name azureadsiginlogs \
+--role $ROLE --handler signinlogs.lambda_handler \
+--runtime python3.6 --timeout 300 --zip-file fileb://lambda-signin.zip --region $REGION
+
+aws lambda create-function --function-name azureadauditlogs \
+--role $ROLE --handler auditlogs.lambda_handler \
+--runtime python3.6 --timeout 300 --zip-file fileb://lambda-audit.zip --region $REGION
+
+## Create secure parameters
+##
+
+aws ssm put-parameter --name AzureGraphAPIClientID \
+--description "Azure AD Client ID used for Azure AD Log Lambdas" \
+--type "SecureString" --value $CLIENTID \
+--key-id "alias/azureadlogs" --region $REGION
+
+aws ssm put-parameter --name AzureGraphAPIClientSecret \
+--description "Azure AD Client Secret used for Azure AD Log Lambdas" \
+--type "SecureString" --value $CLIENTSECRET \
+--key-id "alias/azureadlogs" --region $REGION
---
+++ signinlogs.py
@@ -0,0 +1,144 @@
+import json
+import time
+import datetime
+import boto3
+import requests
+import re
+from adal import AuthenticationContext
+
+print('Loading function')
+
+
+def obtain_accesstoken(tenantname,clientid,clientsecret):
+    auth_context = AuthenticationContext('https://login.microsoftonline.com/' +
+        tenantname)
+    token = auth_context.acquire_token_with_client_credentials(
+        resource="https://graph.microsoft.com",client_id=clientid,
+        client_secret=clientsecret)
+    return token
+    
+def makeapirequest(endpoint,token,q_param=None):
+    ## Create a valid header using the provided access token
+    ##
+        
+    headers = {'Content-Type':'application/json', \
+    'Authorization':'Bearer {0}'.format(token['accessToken'])}
+           
+    ## This section handles a bug with the Python requests module which
+    ## encodes blank spaces to plus signs instead of %20.  This will cause
+    ## issues with OData filters
+    
+    if q_param != None:
+        response = requests.get(endpoint,headers=headers,params=q_param)
+        print('Request made to ' + response.url)
+    else:
+        response = requests.get(endpoint,headers=headers)
+        print('Request made to ' + response.url)
+    if response.status_code == 200:
+        json_data = json.loads(response.text)
+            
+        ## This section handles paged results and combines the results 
+        ## into a single JSON response.  This may need to be modified
+        ## if results are too large
+
+        if '@odata.nextLink' in json_data.keys():
+            record = makeapirequest(json_data['@odata.nextLink'],token)
+            entries = len(record['value'])
+            count = 0
+            while count < entries:
+                json_data['value'].append(record['value'][count])
+                count += 1
+        return(json_data)
+    else:
+        raise Exception('Request failed with ',response.status_code,' - ',
+            response.text)
+
+    
+def lambda_handler(event, context):
+
+    try:
+        print('Attempting to contact Parameter Store...')
+        
+        ## Get encrypted parameters from parameter store
+        ##
+        client = boto3.client('ssm')
+        response = client.get_parameters(
+            Names=[
+                "AzureGraphAPIClientID",
+                "AzureGraphAPIClientSecret"
+            ],
+            WithDecryption=True
+        )
+        
+        for parameter in response['Parameters']:
+            if parameter['Name'] == "AzureGraphAPIClientID":
+                clientid = parameter['Value']
+            elif parameter['Name'] == "AzureGraphAPIClientSecret":
+                clientsecret = parameter['Value']
+        
+        ## Get unencrypted parameters from parameter store
+        ##
+        client = boto3.client('ssm')
+        response = client.get_parameters(
+            Names=[
+                "AzureGraphAPITenantName",
+                "AzureGraphAPISignInEndpoint",
+                "AzureGraphAPIBucket",
+                "AzureGraphAPISignInDays"
+            ],
+            WithDecryption=False
+        )
+    
+        for parameter in response['Parameters']:
+            if parameter['Name'] == "AzureGraphAPITenantName":
+                tenantname = parameter['Value']
+            elif parameter['Name'] == "AzureGraphAPISignInEndpoint":
+                endpoint = parameter['Value']
+            elif parameter['Name'] == "AzureGraphAPIBucket":
+                bucket = parameter['Value']
+            elif parameter['Name'] == "AzureGraphAPISignInDays":
+                logdays = parameter['Value']
+    
+        ## Obtain current date and create OData filter
+        ##
+    
+        todaydate = (datetime.datetime.now()).strftime("%Y-%m-%d")
+        yesterdaydate = (datetime.datetime.today() - datetime.timedelta(days=int(logdays))).strftime("%Y-%m-%d")
+        filter = "createdDateTime gt " + yesterdaydate + " and createdDateTime lt " + todaydate
+        params = { "$filter":filter}
+    
+        print('Attempting to obtain an access token...')
+    
+        ## Obtain bearer token from Azure AD
+        ##
+    
+        token = obtain_accesstoken(tenantname,clientid,clientsecret)
+
+    
+        ## Submit request to Graph API
+        ##
+    
+        data = makeapirequest(endpoint,token,q_param=params)
+        
+        if '@odata.context' in data.keys():
+            del data['@odata.context']
+        elif '@odata.nextLink' in data.keys():
+            del data['@odata.nextLink']
+            
+        ## Cleanup the sign in logs
+        ##
+    
+        for record in data['value']:
+            record['createdDateTime'] = re.sub('[TZ]',' ', record['createdDateTime'])
+    
+        ## Encode string
+        ##
+        string = json.dumps(data)
+        encoded_string = string.encode("utf-8")
+    
+        print("Writing file to s3...")
+        s3_path = "aadsigninlogs/" + todaydate + "-" + yesterdaydate + ".json"
+        s3 = boto3.resource('s3')
+        s3.Bucket(bucket).put_object(Key=s3_path, Body=encoded_string)
+    except Exception as e:
+        print("Expection thrown: {}".format(e))
